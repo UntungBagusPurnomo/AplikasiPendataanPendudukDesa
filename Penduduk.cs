6cd6839e0d9355ee23418c0f93c1730ddd6afc48
UntungBagusPurnomo <60880422+UntungBagusPurnomo@users.noreply.github.com>
2021-01-30 19:37:11 +0700

    Add files via upload

---
+++ Penduduk.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace TestService
+{
+    public class DataPendatang
+    {
+        public string id_pendatang;
+        public string nik;
+        public string nama;
+        public string tempat_lahir;
+        public string tanggal_lahir;
+        public string jenis_kelamin;
+        public string alamat_asal;
+        public string alamat_domisili;
+        public string status;
+
+
+        public void GetDataPendatang()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetAllDataPendatang()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteDataPendatang()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddDataPendatang()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateDataPendatang()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SearchPendatang()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class DataPenduduk
+    {
+        public string id_penduduk;
+        public string nik;
+        public string nama;
+        public string tempat_lahir;
+        public string tanggal_lahir;
+        public string jenis_kelamin;
+        public string alamat;
+        public string status;
+
+        public void GetDataPenduduk()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetAllDataPenduduk()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteDataPenduduk()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddDataPenduduk()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateDataPenduduk()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SearchPenduduk()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
