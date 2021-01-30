a93541de104991de1ece183707496b9f8305ff0b
UntungBagusPurnomo <60880422+UntungBagusPurnomo@users.noreply.github.com>
2021-01-30 19:38:07 +0700

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace TestService
+{
+    public class User
+    {
+        public int id_user;
+        public int nama;
+        public int userName;
+        public int password;
+
+        public void AddUser()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Admin
+    {
+        public int id_admin;
+        public int nama;
+        public int userName;
+        public int password;
+
+        public void AddAdmin()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
